953f792538e23796c71606b0f74759fd148c401c
oscar munene <oscarmunene62@gmail.com>
2025-07-05 13:31:30 +0000

    1. add the back to top function in between the  next and previous button

--- src/components/BibleReaderPage.tsx
+++ src/components/BibleReaderPage.tsx
@@ -315,27 +315,6 @@ export function BibleReaderPage({ verseToRead, onReadComplete }: BibleReaderPage
           {selectedBook && selectedChapter ? `${selectedBook} ${selectedChapter}` : 'Select Book'}
         </Button>
         <div className="flex items-center gap-1">
-           <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
-                  <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                  <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                  <span className="sr-only">Toggle theme</span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-
-            <Link href="/settings">
-              <Button variant="outline" size="icon" asChild>
-                <div><Settings className="h-4 w-4" /><span className="sr-only">Settings</span></div>
-              </Button>
-            </Link>
-
             <Popover>
               <PopoverTrigger asChild>
                 <Button variant="outline" size="icon">
